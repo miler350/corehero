ed58ab61953d740118ec80ee43e23257097a2ce7
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-09-07 22:28:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoreheroFirst::Application.config.secret_key_base = 'e9c66b2ab63caeb44d9719625370f7ec65007e6c939892d477fdec8086c8d1e90aab4f6644471e63b6b51192e51e5953b61c300ead6ec07608db4aa82ab714ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoreheroFirst::Application.config.session_store :cookie_store, key: '_corehero-first_session'
